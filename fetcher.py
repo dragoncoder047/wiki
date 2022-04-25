da1fcecf69d83dc78f4b8c39bd1e40587211fe23
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-04-25 15:13:25 -0400

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -5,9 +5,9 @@ rules_to_fetch = [
 ]
 for rule in rules_to_fetch:
     print('fetching rule', rule)
-    resp = requests.get(f'https://www.conwaylife.com/wiki/index.php?title=Rule:{rule}&action=raw')
+    resp = requests.get(f'https://www.conwaylife.com/w/index.php?title=Rule:{rule}&action=raw')
     with open(f'Rule:{rule}', 'w') as rf:
         rf.write(resp.text)
     if len(resp.text) == 0:
-        raise EOFError(f'got nothin\' for {rule}')
+        raise EOFError(f'Error: Rule \'{rule}\' is empty')
 print('Fetched:', rules_to_fetch)
