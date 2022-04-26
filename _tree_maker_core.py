f66a7154c78b6cee8b281457511fbfc56b7f04e3
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-04-26 12:07:22 -0400

    add helpers

--- _tree_maker_core.py
+++ _tree_maker_core.py
@@ -2,6 +2,60 @@
 
 from textwrap import dedent
 
+def has_orthagonal(n, s):
+    #NW, NE, SW, SE, N, W, E, S, C
+    for i in (4, 5, 6, 7):
+        if n[i] == s: return True
+    return False
+
+def has_diagonal(n, s):
+    #NW, NE, SW, SE, N, W, E, S, C
+    for i in (0, 1, 2, 3):
+        if n[i] == s: return True
+    return False
+
+def rotate(t, by=1):
+    for i in range(by):
+        t = t[1:] + (t[0],)
+    return t
+
+def has_orth_seq(n, seq):
+    #NW, NE, SW, SE, N, W, E, S, C
+    orths = n[4:8]
+    for i in range(4):
+        if rotate(orths, i) == seq:
+            return True
+    return False
+
+def has_diag_seq(n, seq):
+    #NW, NE, SW, SE, N, W, E, S, C
+    raise SystemExit('todo')
+    diags = n[0:4]
+    for i in range(4):
+        if rotate(diags, i) == seq:
+            return True
+    return False
+
+def has_3side(n, side, c=True, cc=True):
+    #0,  1,  2,  3,  4, 5, 6, 7
+    #NW, NE, SW, SE, N, W, E, S, C
+    sides = tuple()
+    clockwise = ( # going clockwise
+        (0, 4, 1),
+        (1, 6, 3),
+        (3, 7, 2),
+        (2, 5, 0)
+    )
+    if c:
+        sides += clockwise
+    if reflect:
+        sides += tuple(*(tuple(*reversed(p)) for p in clockwise))
+    for i1, i2, i3 in sides:
+        if (n[i1], n[i2], n[i3]) == side:
+            return True
+    return False
+
+
 def getfilename(rule):
     return f'Rule:{rule}'
     
@@ -9,9 +63,12 @@ def rule(name, numStates, numNeighbors):
     '''
     @rule('Foo', numStates=5, numNeighbors=8)
     def function(n):
-        """Rule description here.
-    @COLORS
-    You can include a colors section."""
+        """
+        Rule description here.
+        @COLORS
+        You can include a colors section.
+        It will automatically be dedented.
+        """
         return something()
 
     For vonNeumann neighborhood, inputs are: N,W,E,S,C
