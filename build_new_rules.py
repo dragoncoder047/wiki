ae433a4affd63e11b5f412af1f0a23d1bc792e0d
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-10 08:04:10 -0400

    remove flags

--- build_new_rules.py
+++ build_new_rules.py
@@ -6,7 +6,7 @@ files = glob.glob('*.ruel')
 
 for file in files:
     rulename = file.rstrip('.ruel')
-    exitcode = os.system(f'python3 -m nutshell transpile -vvvvsc {file} .')
+    exitcode = os.system(f'python3 -m nutshell transpile {file} .')
     if exitcode > 0:
         sys.exit(exitcode)
     os.system(f'mv {rulename}.rule "Rule:{rulename}"')
