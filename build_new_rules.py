ac2d3fc579cdc76288387271fae3ddaffed3955c
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-10 12:55:24 -0400

    fix for https://github.com/supposedly/nutshell/issues/20#issuecomment-1122631816

--- build_new_rules.py
+++ build_new_rules.py
@@ -7,10 +7,11 @@ files = glob.glob('*.ruel')
 for file in files:
     rulename = file.rstrip('.ruel')
     exitcode = os.system(f'python3 -m nutshell transpile {file} .')
+    exitcode += os.system(f'mv {rulename}.rule "Rule:{rulename}"')
     if exitcode > 0:
         sys.exit(exitcode)
-    os.system(f'mv {rulename}.rule "Rule:{rulename}"')
-    
+
+sys.exit(0)
 # permanent fix for supposedly/nutshell#20
 with open('Rule:Fusion') as f: badtext = f.read()
 bef, af = badtext.split('var any.0')
