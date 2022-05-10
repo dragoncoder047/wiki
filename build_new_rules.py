41e74061d8fbfa356b95d50473bfb9c592c5e2d7
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-10 08:39:50 -0400

    permanent fix for supposedly/nutshell#20
    manually patch in missing variable

--- build_new_rules.py
+++ build_new_rules.py
@@ -10,3 +10,10 @@ for file in files:
     if exitcode > 0:
         sys.exit(exitcode)
     os.system(f'mv {rulename}.rule "Rule:{rulename}"')
+    
+# permanent fix for supposedly/nutshell#20
+with open('Rule:Fusion') as f: badtext = f.read()
+bef, af = badtext.split('var any.0')
+goodtext = bef + '\n\nvar anyNTAAHead.0 = {7,8}\nvar any.0' + af
+with open('Rule:Fusion', 'w') as f: f.write(goodtext)
+
