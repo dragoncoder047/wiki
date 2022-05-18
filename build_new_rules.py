8491d792a7f85bab7f0202871e9b1fab2807d57b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-18 10:38:17 -0400

    tweak colors, fix build script

--- build_new_rules.py
+++ build_new_rules.py
@@ -7,14 +7,11 @@ files = glob.glob('*.ruel')
 for file in files:
     rulename = file.rstrip('.ruel')
     exitcode = os.system(f'python3 -m nutshell transpile {file} .')
-    exitcode += os.system(f'mv {rulename}.rule "Rule:{rulename}"')
     if exitcode > 0:
         sys.exit(exitcode)
-
-sys.exit(0)
-# permanent fix for supposedly/nutshell#20
-with open('Rule:Fusion') as f: badtext = f.read()
-bef, af = badtext.split('var any.0')
-goodtext = bef + '\n\nvar anyNTAAHead.0 = {7,8}\nvar any.0' + af
-with open('Rule:Fusion', 'w') as f: f.write(goodtext)
+    try:
+        os.remove(f"Rule:{rulename}")
+    except FileNotFoundError:
+        pass
+    os.rename(f'{rulename}.rule', f"Rule:{rulename}")
 
